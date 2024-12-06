b732da234e66443ea97eafeffb20551866ab4e38
xiaolipin <lipin_xiao@wdkingtech.com>
2024-12-07 01:13:59 +0800

    [新增] [9]Palindrome Number 完成

---
+++ src/leetcode/editor/en/[9]Palindrome Number.ts
@@ -0,0 +1,54 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: x = -121
+//Output: false
+//Explanation: From left to right, it reads -121. From right to left, it 
+//becomes 121-. Therefore it is not a palindrome.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: x = 10
+//Output: false
+//Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// 
+//Follow up: Could you solve it without converting the integer to a string?
+//
+// Related Topics Math 👍 13272 👎 2779
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+function isPalindrome(x: number): boolean {
+    let s = x.toString();
+    let sr = s.split('');
+    let sl = [...sr].reverse();
+    for (let i = 0; i < sr.length; i++) {
+        if (sr[i] !== sl[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+//leetcode submit region end(Prohibit modification and deletion)
